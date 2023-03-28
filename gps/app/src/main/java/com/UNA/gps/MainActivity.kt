478daafa06293ce9b9710a4b05f624227730d9bb
Jeff Vargas Barrantes <jeff.vargas.barrantes@est.una.ac.cr>
2023-03-27 20:19:05 -0600

    menu toggle

--- gps/app/src/main/java/com/UNA/gps/MainActivity.kt
+++ gps/app/src/main/java/com/UNA/gps/MainActivity.kt
@@ -2,10 +2,35 @@ package com.UNA.gps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var drawableLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+
+        //assign toolbar to main
+        setSupportActionBar(toolbar)
+
+        drawableLayout = findViewById(R.id.drawerlayout)
+        var toggle = androidx.appcompat.app.ActionBarDrawerToggle(
+            this, drawableLayout, toolbar, R.string.open, R.string.close)
+        drawableLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
     }
+
+    override fun onBackPressed() {
+        if(drawableLayout.isDrawerOpen(GravityCompat.START)){
+            drawableLayout.closeDrawer(GravityCompat.START)
+        }else{
+            super.onBackPressed()
+        }
+    }
+
 }
