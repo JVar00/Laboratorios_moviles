179dabecb370d68de68af8192a04138666e0ed5b
Jeff Vargas Barrantes <jeff.vargas.barrantes@est.una.ac.cr>
2023-03-30 15:22:39 -0600

    init_fragments

--- gps/app/src/main/java/com/UNA/gps/MainActivity.kt
+++ gps/app/src/main/java/com/UNA/gps/MainActivity.kt
@@ -2,10 +2,13 @@ package com.UNA.gps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var drawableLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +34,21 @@ class MainActivity : AppCompatActivity() {
         }else{
             super.onBackPressed()
         }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+when(item.itemId){
+            R.id.menu_item_home -> {
+                Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+                //supportFragmentManager.beginTransaction().replace(R.id.fragment_container, HomeFragment()).commit()
+            }
+            R.id.menu_item_maps -> {
+                Toast.makeText(this, "Maps", Toast.LENGTH_SHORT).show()
+                //supportFragmentManager.beginTransaction().replace(R.id.fragment_container, MapsFragment()).commit()
+            }
+        }
+        drawableLayout.closeDrawer(GravityCompat.START)
+        return true
     }
 
 }
